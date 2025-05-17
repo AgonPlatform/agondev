1cf2220f44ec64b6f250f187a053c9ce33d797cd
Jeroen Venema <jeroen.venema@gmail.com>
2025-05-17 14:46:15 +0200

    VDP API code fixes

--- src/lib/agon/vdp_move_graphics_origin_and_viewport.c
+++ src/lib/agon/vdp_move_graphics_origin_and_viewport.c
@@ -4,6 +4,6 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-static VDU_A_B_CMD vdu_move_graphics_origin_and_viewport = { 23, 0, 0x9C };
+static VDU_A_B_CMD vdu_move_graphics_origin_and_viewport = { 23, 0, 0x9F };
 
 void vdp_move_graphics_origin_and_viewport( void ) { VDP_PUTS( vdu_move_graphics_origin_and_viewport ); }
