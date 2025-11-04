d6904518687d300cbb0ed029eb6de44b21c11293
Richard Turnnidge <117299012+richardturnnidge@users.noreply.github.com>
2025-11-04 20:13:38 +0000

    added animated sprite frames

--- release/examples/simple_sprites/readme.md
+++ release/examples/simple_sprites/readme.md
@@ -2,5 +2,6 @@
 
 Loads a bitmap from memory and displays on screen as a sprite.
 Moves around to show independence from background graphic.
+Loads bitmap sequence for animating sprite frames.
 
 Press ESC to exit the program.
--- release/examples/simple_sprites/src/main.c
+++ release/examples/simple_sprites/src/main.c
@@ -2,6 +2,7 @@
 Example of:
   1) loading simple bitmap from memory into a sprite
   2) moving sprite around screen
+  3) animating sprite frames
 
 vdp calls used:
 void vdp_select_bitmap( int n );
@@ -16,6 +17,8 @@ void vdp_show_sprite( void );
 void vdp_move_sprite_to( int x, int y );
 void vdp_refresh_sprites( void );
 void vdp_reset_sprites( void );
+void vdp_next_sprite_frame( void );
+void vdp_create_sprite( int sprite, int bitmap_num, int frames );
 */
 
 #include <stdint.h>
@@ -25,23 +28,30 @@ void vdp_reset_sprites( void );
 #include <agon/vdp_keyboard.h>
 #include <agon/timer.h>
 
-#include "bod0.h"
-
+#include "man.h"
+#include "gem.h"
 
 const uint8_t screen_mode = 8;
 const int delay_ms = 20;
-const int bitmap_ID = 0;
-const int sprite_ID = 0;
-const int numSprites = 1;
+const int man_bitmap_ID = 10;
+const int gem_sprite_start_bitmap_ID = 0;
+const int man_sprite_ID = 0;
+const int gem_sprite_ID = 1;
+const int numSprites = 2;
+const int gem_frames = 6;
+
 
 uint16_t x = 0;
 uint16_t y = 0;
 int8_t dx = 2;
 int8_t dy = 1;
+int8_t gem_anim_count = 0;
+
 
 int main(void) {
 
   vdp_mode(screen_mode);
+  vdp_set_pixel_coordinates();
   vdp_cursor_enable(false);
   vdp_set_text_colour( 15 );
   vdp_set_text_colour( 128 + 8 );
@@ -49,7 +59,6 @@ int main(void) {
   vdp_reset_sprites();
   printf("AgonDev Simple Sprite Example\n\n");
   printf("Press ESC to exit");
-
 
   //draw some background tringles to illustrate alpha channel
   vdp_set_graphics_fg_colour( 0, GREEN );
@@ -61,29 +70,62 @@ int main(void) {
   vdp_set_graphics_fg_colour( 0, RED );
   vdp_filled_triangle( 120,30 ,90,150 , 300, 170 );
 
-  // load bitmap and setup sprite
-  vdp_select_bitmap(bitmap_ID);
-  vdp_load_bitmap( 16, 16, bod0 );
+  // load man bitmap and setup sprite
+  vdp_select_bitmap(man_bitmap_ID);
+  vdp_load_bitmap( 16, 16, man_bitmap );
 
-  vdp_select_sprite(sprite_ID);
+  vdp_select_sprite(man_sprite_ID);
   vdp_clear_sprite();
-  vdp_add_sprite_bitmap(bitmap_ID);
-
+  vdp_add_sprite_bitmap(man_bitmap_ID);
   vdp_show_sprite();
+
+  // load bitmaps for animated gem sprite, all in gem.h file
+  vdp_select_bitmap(gem_sprite_start_bitmap_ID);
+  vdp_load_bitmap( 16, 16, blue1 );
+  vdp_select_bitmap(gem_sprite_start_bitmap_ID + 1);
+  vdp_load_bitmap( 16, 16, blue2 );
+  vdp_select_bitmap(gem_sprite_start_bitmap_ID + 2);
+  vdp_load_bitmap( 16, 16, blue3 );
+  vdp_select_bitmap(gem_sprite_start_bitmap_ID + 3);
+  vdp_load_bitmap( 16, 16, blue4 );
+  vdp_select_bitmap(gem_sprite_start_bitmap_ID + 4);
+  vdp_load_bitmap( 16, 16, blue5 );
+  vdp_select_bitmap(gem_sprite_start_bitmap_ID + 5);
+  vdp_load_bitmap( 16, 16, blue6 );
+
+  // create sprite with 6 frames of loaded bitmaps
+  vdp_select_sprite(gem_sprite_ID);
+  vdp_clear_sprite();
+  vdp_create_sprite( gem_sprite_ID, gem_sprite_start_bitmap_ID, 6 );
+  vdp_move_sprite_to( 150,110 );
+  vdp_show_sprite();
+
+  // activate the sprites
   vdp_activate_sprites(numSprites);
 
   //move sprite around until ESC key pressed
   while(true) {
     delay(delay_ms);
+    vdp_select_sprite(man_sprite_ID);
     vdp_move_sprite_to( x, y );
-    vdp_refresh_sprites();
+
     x += dx;
     if (x>300) dx = -1;
     if (x<1) dx = 1;
     y += dy;
     if (y>220) dy = -1;
     if (y<1) dy = 1;
-    
+   
+    // uses a counter so animation is not as fast as man moving
+    gem_anim_count++;
+    if(gem_anim_count > 10){
+      vdp_select_sprite(gem_sprite_ID);
+      vdp_next_sprite_frame();
+      gem_anim_count=0;
+    }
+
+    vdp_refresh_sprites();
+
     if(vdp_getKeyCode() == 27) break;
   }
 
