730f6d888c1325a297bcff26cb3d87f40adf7e2f
Richard Turnnidge <117299012+richardturnnidge@users.noreply.github.com>
2025-11-06 20:42:20 +0000

    fixed display issues
    updated after vdp fix

--- release/examples/fonts/src/main.c
+++ release/examples/fonts/src/main.c
@@ -1,25 +1,6 @@
 /*
 Example of:
-  1) creating User Fonts
-  
-  
-  // VDU 23, 0, &95, 0, bufferId; flags: Select font
-void vdp_font_select( int buffer_id );
-// VDU 23, 0, &95, 1, bufferId; width, height, ascent, flags: Create font from buffer
-void vdp_font_create( int buffer_id, int width, int height, int ascent, int flags );
-
-void vdp_adv_write_block(int bufferID, int length);
-void vdp_adv_write_block_data(int bufferID, int length, char *data);
-
-void vdp_adv_call_buffer(int bufferID);
-// Command 2: Clear a buffer
-// 	VDU 23, 0 &A0, bufferId; 2
-void vdp_adv_clear_buffer(int bufferID);
-// Command 3: Create a writeable buffer
-// 	VDU 23, 0 &A0, bufferId; 3, length;
-void vdp_adv_create(int bufferID, int length);
-// VDU 23, 0, &95, 5, bufferId;: Copy system font to buffer
-void vdp_font_copy( int buffer_id );
+  1) Defining 3 differnt sized user fonts
 */
 
 #include <stdint.h>
@@ -27,7 +8,7 @@ void vdp_font_copy( int buffer_id );
 #include <stdbool.h>
 #include <agon/vdp.h>
 #include <agon/vdp_keyboard.h>
-
+#include <agon/timer.h>
 #include "user_fonts.h"
 #include "arial_bold.h"
 
@@ -42,72 +23,52 @@ const int megaFontDataSize = 8192;
 int main(void) {
 
 // setup fonts
+  vdp_adv_clear_buffer(-1);
 
 // make digital_8x8 font
-  vdp_adv_create(smallFontBufferID, fontDataSize);
-  vdp_adv_clear_buffer(smallFontBufferID);
   vdp_adv_write_block_data(smallFontBufferID, fontDataSize, digital_8x8);
   vdp_font_create( smallFontBufferID, 8, 8, 8, 0 );
 
 // make tremolo_8x16 font
-  vdp_adv_create(tallFontBufferID, fontDataSize);
-  vdp_adv_clear_buffer(tallFontBufferID);
   vdp_adv_write_block_data(tallFontBufferID, tallFontDataSize, tremolo_8x16);
   vdp_font_create( tallFontBufferID, 8, 16, 16, 0 );
 
-// make 16x16 arial_bold font
-  vdp_adv_create(megaFontBufferID, megaFontDataSize);
-  vdp_adv_clear_buffer(megaFontBufferID);
+// make 16x16 arial_bold font, but use 14 pixels for width
   vdp_adv_write_block_data(megaFontBufferID, megaFontDataSize, arial_bold);
-  vdp_font_create( megaFontBufferID, 12, 16, 16, 0 );
+  vdp_font_create( megaFontBufferID, 14, 16, 16, 0 ); 
   
+// start program here
 
-  // start program here
-
-  vdp_font_select( -1 ); // make sure on default system font
-
+  vdp_font_select( -1 ,0); // make sure on default system font
   vdp_cursor_enable(false);
   vdp_clear_screen();
 
-  printf("AgonDev User Font Example\n\n"); // this prints at top of screen ok
+  printf("AgonDev User Font Example\n\n"); 
   printf("Press ESC to Exit\n");
 
+// select small user font and print something
+  vdp_font_select( smallFontBufferID ,0);
+  vdp_cursor_tab(10,15);
+  printf("This is a retro 8x8 futuristic font"); 
 
-  vdp_cursor_tab(30,5);
-  printf("First msg\n");        // this works
-
-  // select small user font and print something
-  vdp_font_select( smallFontBufferID );
-  vdp_cursor_tab(5,20);
-  printf("This is a retro 8x8 futuristic font"); // this fails to go to correct position
-
- // select tall user font and print something
-  vdp_font_select( tallFontBufferID );
-  printf("This is a TALL 8x16 sketchy font"); // this loses first char
-
- // select tall user font and print something
-  vdp_font_select( megaFontBufferID );
-  printf("This is Mega Arial Bold");        // this loses first char
+// select tall user font and print something
+  vdp_font_select( tallFontBufferID,0 );
+  vdp_cursor_tab(10,10);  
+  printf("This is a TALL 8x16 sketchy font"); 
 
 
-  vdp_font_select( -1 ); // reset to system font
+// select tall user font and print something
+  vdp_font_select( megaFontBufferID ,0);
+  vdp_cursor_tab(5,14); 
+  printf("This is 16x16 Arial Bold Style");  
 
-  // testing positions
-  vdp_cursor_tab(5,5);  // this fails to go to correct position
-  printf("msg at 5,5");
-
-  vdp_cursor_tab(10,20); // this works
-  printf("msg at 10,20");
-
-  vdp_cursor_tab(20,30); // this works
-  printf("msg  at 20,30");
 
   while(true) {
     if(vdp_getKeyCode() == 27) break;
   }
 
   // reset everything
-  vdp_font_select( -1 );
+  vdp_font_select( -1 ,0);
   vdp_cursor_enable(true);
   vdp_clear_screen();
   return 0; 
