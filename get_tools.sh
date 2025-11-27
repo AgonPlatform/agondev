47de1d693d5e3a86e3647c050338b667bf7c6660
Jeroen Venema <jeroen.venema@gmail.com>
2025-11-27 20:52:07 +0100

    Multi-platform safe CURL download option in get_tools

--- get_tools.sh
+++ get_tools.sh
@@ -1,49 +1,32 @@
 #!/bin/bash
-
-# Immediately stop on any error
 set -e
 
-#Repository
-REPO=https://api.github.com/repos/AgonPlatform/agondev
+# Environment
+OS_NAME="$(uname -s | tr 'A-Z' 'a-z')"    # darwin or linux
+ARCH="$(uname -m | tr 'A-Z' 'a-z')"       # arm64, x86_64, etc.
 
-#Environment
-OS_NAME="$(uname -s | tr 'A-Z' 'a-z')"
-ARCH="$(uname -m | tr 'A-Z' 'a-z')"
-
-#Directories
-RELEASE_BIN_DIR=./release/bin
-DOWNLOAD=./download
 ASSET="agondev-${OS_NAME}_${ARCH}.tar.gz"
 
-#Check tools
-if ! command -v curl &> /dev/null
-then
-    echo "Error: curl is not installed or not in PATH."
-    exit 1
-fi
-if ! command -v jq &> /dev/null
-then
-    echo "Error: jq is not installed or not in PATH."
-    exit 1
-fi
+# Directories
+RELEASE_BIN_DIR=./release/bin
+DOWNLOAD=./download
 
-echo [ Downloading $ASSET ]
+mkdir -p "$DOWNLOAD"
+cd "$DOWNLOAD"
 
-mkdir -p $DOWNLOAD
-cd $DOWNLOAD
-curl -s $REPO/releases/latest \
-  | jq -r --arg name "$ASSET" '.assets[] | select(.name==$name) | .browser_download_url' \
-  | xargs -n 1 curl -LO
+echo "[ Downloading $ASSET ]"
+URL="https://github.com/AgonPlatform/agondev/releases/latest/download/$ASSET"
 
-if [ ! -f "$ASSET" ]; then
-    echo "Error: '$ASSET' does not exist in the latest release" >&2
-    exit 1
-fi
+# Download the asset, following redirects
+curl -L -o "$ASSET" "$URL"
 
-echo [ Unpacking tools ]
-tar xf $ASSET
+echo "[ Unpacking tools ]"
+tar xf "$ASSET"
 cd ..
-rm -rf ./release/bin
-echo [ Copying tools to release ]
-cp -R $DOWNLOAD/agondev/bin $RELEASE_BIN_DIR
-echo [ Done ]
+
+rm -rf "$RELEASE_BIN_DIR"
+echo "[ Copying tools to release ]"
+cp -R "$DOWNLOAD/agondev/bin" "$RELEASE_BIN_DIR"
+
+echo "[ Done ]"
+
