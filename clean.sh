38892f66b3e65b4faf33a3c59a49da42a7caa52b
Jeroen Venema <jeroen.venema@gmail.com>
2025-11-27 19:46:52 +0100

    Local clean script

---
+++ clean.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Immediately stop on any error
+set -e
+
+echo "[ Cleaning up ]"
+rm -f *.tar.gz
+rm -f hexload-send.spec
+rm -rf build
+rm -rf dist
+rm -rf download
+rm -rf llvm-build
+rm -rf llvm-project
+rm -rf venv
+rm -rf release/bin
+rm -rf release/lib
+echo "[ Done ]"
