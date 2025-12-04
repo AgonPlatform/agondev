6a5ac72285dd0beb95f60f7a7638d3ad80ece726
Jeroen Venema <jeroen.venema@gmail.com>
2025-12-04 16:46:08 +0100

    Fix for issue #36 - vdp_capture_bitmap()

--- src/lib/libvdp/vdp_capture_bitmap.c
+++ src/lib/libvdp/vdp_capture_bitmap.c
@@ -4,13 +4,13 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-static VDU_A_B_CMD_B_B_B	vdu_capture_bitmap = { 23, 27, 1, 0, 0 };
+static VDU_A_B_CMD_W_W	vdu_capture_bitmap = { 23, 27, 1, 0, 0 };
 
 // VDU 23, 27, 1, n, 0, 0;: Capture screen data into bitmap n
 void vdp_capture_bitmap(uint16_t top, uint16_t left, uint16_t bottom, uint16_t right, uint8_t bitmapID) {
     vdp_move_to(left, top);
     vdp_move_to(right, bottom);
-    vdu_capture_bitmap.b0 = bitmapID;
+    vdu_capture_bitmap.w0 = bitmapID; // VDP API only used LSB
 	VDP_PUTS( vdp_capture_bitmap );
 }
 
