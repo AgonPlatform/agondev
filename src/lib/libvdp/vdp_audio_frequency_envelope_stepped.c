336996dd5227ce56b683893771c61afaef9b6853
Richard Turnnidge <117299012+richardturnnidge@users.noreply.github.com>
2026-04-12 17:50:20 +0100

    Update vdp_audio_frequency_envelope_stepped.c
    Changed:
    - the function to have extra parameter (pointer to array)
    - extra for/next loop to send that data.

--- src/lib/libvdp/vdp_audio_frequency_envelope_stepped.c
+++ src/lib/libvdp/vdp_audio_frequency_envelope_stepped.c
@@ -8,7 +8,7 @@ static VDU_AUDIO_CMD_B_B_B_W vdu_audio_b_b_b_w = { 23, 0, 0x85, 0, 7, 1, 0, 0, 0
 
 //	Type 1: Stepped frequency envelope
 //		VDU 23, 0, &85, channel, 7, 1, phaseCount, controlByte, stepLength; [phase1Adjustment; phase1NumberOfSteps; phase2Adjustment; phase2NumberOfSteps; ...]
-void vdp_audio_frequency_envelope_stepped( int channel, int phaseCount, int controlByte, int stepLength )
+void vdp_audio_frequency_envelope_stepped( int channel, int phaseCount, int controlByte, int stepLength, const int16_t* envelopeData )
 {
 	vdu_audio_b_b_b_w.channel = channel;
 	vdu_audio_b_b_b_w.CMD = 7;
@@ -17,4 +17,10 @@ void vdp_audio_frequency_envelope_stepped( int channel, int phaseCount, int cont
 	vdu_audio_b_b_b_w.b3 = controlByte;
 	vdu_audio_b_b_b_w.w4 = stepLength;
        	VDP_PUTS( vdu_audio_b_b_b_w ); 
+
+	// send envelope data, should get 2 x envelopeSize in array passed: phaseAdjustment & phaseNumSteps
+    // each item in array is 16bit signed number
+    for(uint8_t wordsToSend = 0; wordsToSend < phaseCount * 2 ; wordsToSend ++){
+        VDP_PUTS(envelopeData[wordsToSend]);
+    }
 }
